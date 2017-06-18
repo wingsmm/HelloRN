f8067602dc91754d56170c9a01b0857aafdc61a5
ZhangBo <wingsmm@me.com>
2017-06-18 19:41:34 +0800

    HelloRN

---
+++ index.ios.js
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import {
+  AppRegistry,
+} from 'react-native';
+
+import WeatherProjectPart1 from './WeatherProject_Part1';
+import WeatherProject from './WeatherProject';
+
+// AppRegistry.registerComponent('WeatherProject', () => WeatherProject);
+
+AppRegistry.registerComponent('HelloRN', () => WeatherProjectPart1);
